4f17d00ad8e5e45613917759dd51c40fb35c512f
Shinryuzz <mshin1127@gmail.com>
2024-12-30 17:51:21 +0900

    feat: improve echo w/ type safe

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
-import * as line from '@line/bot-sdk';
+import { messagingApi } from "@line/bot-sdk";
+import type { WebhookEvent, MessageEvent, TextMessage } from "@line/bot-sdk";
 
 interface Env {
-	LINE_CHANNEL_ACCESS_TOKEN: string;
+  LINE_CHANNEL_ACCESS_TOKEN: string;
+}
+
+interface WebhookRequestBody {
+  events: WebhookEvent[];
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const client = new line.messagingApi.MessagingApiClient({ channelAccessToken: env.LINE_CHANNEL_ACCESS_TOKEN });
+  async fetch(request, env, ctx): Promise<Response> {
+    const client = new messagingApi.MessagingApiClient({ channelAccessToken: env.LINE_CHANNEL_ACCESS_TOKEN });
 
-		const body = await request.text();
-		const events = JSON.parse(body).events;
+    const { events } = (await request.json()) as WebhookRequestBody;
 
-		await Promise.all(
-			events
-				.filter((e) => e.type === 'message' && e.message.type === 'text')
-				.map((e) =>
-					client.replyMessage({
-						replyToken: e.replyToken,
-						messages: [
-							{
-								type: 'text',
-								text: e.message.text,
-							},
-						],
-					})
-				)
-		);
-
-		return new Response('OK');
-	},
+    await Promise.all(
+      events
+        .filter((e): e is MessageEvent & { message: TextMessage } => e.type === "message" && e.message.type === "text")
+        .map((e) =>
+          client.replyMessage({
+            replyToken: e.replyToken,
+            messages: [
+              {
+                type: "text",
+                text: e.message.text,
+              },
+            ],
+          })
+        )
+    );
+    return new Response("OK");
+  },
 } satisfies ExportedHandler<Env>;
